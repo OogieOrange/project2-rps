c8d1760377339e6ccc50a6ca2ce9b5ccee021bde
OogieOrange <evelinapalffy01@gmail.com>
2023-02-17 13:43:14 +0000

    Reconstruct functions, add computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,10 @@
-function game() {
-
-}
-
-function checkAnswer() {
-
-}
-
-function choices() {
-
-}
-
-function incrementWin() {
-
-}
-
-function incrementLoss() {
-
-}
-
-function reset() {
-
+function compChoices() {
+    let compChoice = Math.floor(Math.random() * 3) + 1;
+    if (compChoice === 1) {
+        return "rock";
+    } else if (compChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
