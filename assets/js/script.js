3285ca97c27b35a350004a45a83b3d7d03a0fab9
OogieOrange <evelinapalffy01@gmail.com>
2023-02-17 14:38:53 +0000

    Add winner calculator function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+let button = document.getElementById("button-box");
+let playChoice = button.getAttribute("data-type");
+
 function compChoices() {
     let compChoice = Math.floor(Math.random() * 3) + 1;
     if (compChoice === 1) {
@@ -6,5 +9,26 @@ function compChoices() {
         return "paper";
     } else {
         return "scissors";
+    }
+}
+
+function winner() {
+    let copmuter = compChoices();
+    let result = playChoice + computer;
+
+    if (playChoice === computer) {
+        compMove.textContent = `${computer}. It's a draw.`;
+    } else if (
+        result = "rockscissors" ||
+        result = "paperrock" ||
+        result = "scissorspaper"
+    ) {
+        compMove.textContent = `${computer}. Congrats! You won!`;
+    } else if (
+        result = "rockspaper" ||
+        result = "paperscissors" ||
+        result = "scissorsrock"
+    ) {
+        compMove.textContent = `${computer}... Sorry. You Lost.`;
     }
 }
